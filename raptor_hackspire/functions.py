3e69f1b491724bfbaa483fd6aac7fce75423f0cf
R Sri Sudarshan <srisudarshanrg@gmail.com>
2025-06-30 23:18:11 +0530

    menu for the canteen admin completed

--- raptor_hackspire/functions.py
+++ raptor_hackspire/functions.py
@@ -1,8 +1,8 @@
 from flask_login import current_user
 from flask import abort
 from functools import wraps
-from .models import Student, Teacher
-from . import bcrypt, db
+from .models import Student, Teacher, Canteen
+from . import bcrypt, db, session
 from flask_login import login_user
 
 def hash_password(password):
@@ -32,19 +32,61 @@ def CreateStudent(username, section, password):
 
         db.session.add(new_student)
         db.session.commit()
-        login_user(new_student)        
+        login_user(new_student)
+        session["user_type"] = "student"       
 
         return "User created successfully", "success"
     except Exception as e:
         print(e)
-        return f"Failed to create user: {e}", "danger"
-    
+        return f"Failed to create student: {e}", "danger"
+
+def CreateTeacher(username, subject, password):
+    try:
+        new_teacher = Teacher(
+            username=username,
+            subject=subject,
+            password=password,
+        )
+
+        db.session.add(new_teacher)
+        db.session.commit()
+
+        login_user(new_teacher)
+        session["user_type"] = "teacher"
+
+        return "Account created succesfully", "success"
+    except Exception as e:
+        print(e)
+        return f"Failed to create teacher: {e}", "danger"
+
 def LoginStudent(username, password):
     student = Student.query.filter_by(username=username).first()
     if student:
         check = check_hash_password(student.password, password)
         if check:
             return student
+        else:
+            return False
+    else:
+        return False
+    
+def LoginTeacher(username, password, subject):
+    teacher = Teacher.query.filter_by(username=username).first()
+    if teacher:
+        check = check_hash_password(teacher.password, password)
+        if check:
+            return teacher
+        else:
+            return False
+    else:
+        return False
+
+def LoginCanteen(username, password):
+    canteen = Canteen.query.filter_by(username=username).first()
+    if canteen:
+        check = check_hash_password(canteen.password, password)
+        if check:
+            return canteen
         else:
             return False
     else:
--- raptor_hackspire/routes.py
+++ raptor_hackspire/routes.py
@@ -1,19 +1,20 @@
-from . import app, session
-from flask_login import login_required, login_user, current_user
+from . import app, session, db
+from flask_login import login_required, login_user, current_user, logout_user
 from flask import render_template, redirect, url_for, request, flash
 from datetime import datetime, date as dt
-from .functions import roles_required, hash_password, check_hash_password, CreateStudent, LoginStudent
-from .user_validations import password_equal_confirm_password, unique_username
+from .functions import roles_required, hash_password, check_hash_password, CreateStudent, LoginStudent, CreateTeacher, LoginTeacher, LoginCanteen
+from .user_validations import password_equal_confirm_password, unique_username_student, unique_username_teacher
+from .models import Student, Teacher, Canteen, Menu, Orders, Assignments
 
 @app.route("/")
 @login_required
 def home():
-    return render_template("home.html", role=current_user.get_role)
+    return render_template("home.html", role=current_user.get_role())
 
-@app.route('/<role>/<username>/Assignments')
+@app.route('/Assignments')
 @login_required
 @roles_required("student", "teacher")
-def assignments(username):
+def assignments(    ):
     """
     format for assignment array(passed to "render_template"):
     array = [assignment, assignment, assignment], where:
@@ -36,6 +37,36 @@ def assignments(username):
 
     return render_template('StudentAssignmentPage.html', empty = "", assignments = assignments)
 
+# student view of canteen
+@app.route("/canteen-buddy", methods=["GET", "POST"])
+@login_required
+@roles_required("student")
+def canteen_buddy():
+    return render_template("canteen_buddy.html", role=current_user.get_role())
+
+# canteen admin view of canteen buddy
+@app.route("/canteen-admin", methods=["GET", "POST"])
+@login_required
+@roles_required("canteen")
+def canteen_admin():
+    menu = Menu.query.all()
+
+    if request.method == "POST":
+        if "menu_id" in request.form:
+            menu_id = request.form.get("menu_id")
+            category = request.form.get("category")
+            item_name = request.form.get("item_name")
+            price = int(request.form.get("price"))
+            availability = request.form.get("availability")
+            m = Menu.query.filter_by(id=menu_id).first()
+            m.category = category
+            m.item_name = item_name
+            m.price = price
+            m.availability = availability
+            db.session.commit
+
+    return render_template("canteen_admin.html", role=current_user.get_role(), menu=menu)
+
 @app.route("/student-login", methods=["GET", "POST"])
 def student_login():
     if request.method == "POST":
@@ -55,10 +86,36 @@ def student_login():
 
 @app.route("/teacher-login", methods=["GET", "POST"])
 def teacher_login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        teacher = LoginTeacher(username, password)
+        if teacher != False:
+            login_user(teacher)
+            session["user_type"] = "teacher"            
+            flash("Logged in sucessfully", "success")
+            return redirect(url_for("home"))
+        else:
+            flash("Incorrect login credentials", "danger")
+
     return render_template("login.html", href="{{urlFor('teacher_register')}}", title="Teacher Login")
 
 @app.route("/canteen-login", methods=["GET", "POST"])
 def canteen_login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        canteen = LoginCanteen(username, password)
+        if canteen != False:
+            login_user(canteen)
+            session["user_type"] = "canteen"            
+            flash("Logged in sucessfully", "success")
+            return redirect(url_for("home"))
+        else:
+            flash("Incorrect login credentials", "danger")
+
     return render_template("login.html", title="Canteen Login")
 
 @app.route("/student-register", methods=["GET", "POST"])
@@ -72,13 +129,17 @@ def student_register():
 
         # validations
         password_validation = password_equal_confirm_password(password, confirm_password)
-        username_validation = unique_username(username)
+        username_validation = unique_username_student(username)
 
         results = [username_validation, password_validation]
         if results[0] == True and results[1] == True:
             hashed = hash_password(password)
             message, category = CreateStudent(username, section, hashed)
             flash(f"{message}", category)
+            if category == "success":
+                redirect(url_for('home'))
+            else:
+                redirect(url_for("student_register"))
         else:
             for i in results:
                 if i != True:
@@ -88,4 +149,33 @@ def student_register():
 
 @app.route("/teacher-register")
 def teacher_register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        subject = request.form.get("subject")
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirm_password")
+
+        password_validation = password_equal_confirm_password(password, confirm_password)
+        username_validation = unique_username_teacher(username)
+
+        results = [username_validation, password_validation]
+
+        if results[0] == True and results[1] == True:
+            hashed = hash_password(password)
+            message, category = CreateTeacher(username, subject, hashed)
+            flash(f"{message}", category)
+            if category == "success":
+                redirect(url_for("home"))
+            else:
+                redirect(url_for("teacher_register"))
+        else:
+            for i in results:
+                if i != True:
+                    flash(f"{i}", category="danger")
+
     return render_template("register.html", section=False, subject=True, title="Teacher Register")
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("student_login"))
